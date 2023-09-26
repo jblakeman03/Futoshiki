3a54df34a1db6674997d607e7db0060e2b61c746
jblakeman03 <jblakeman@murraystate.edu>
2023-09-26 10:17:23 -0500

    Fixed the read in method

--- Futoshiki.py
+++ Futoshiki.py
@@ -2,22 +2,24 @@
 ##Futoshiki Solver 
 
 
-=======
 import os
 numbers = []
 logic = []
 solution = []
 def main():
     number,logic,solution = ReadIn()
+    print(number)
+    print(logic)
+    print(solution)
 
 ##Function to read in values and returns them in 2D Arrays  
 def ReadIn():
     ##Get the file name for the numbers
     ##Will then switch letters to get desired logic and solution files 
-    fileName = input('Input desired number file: ')
-    numberFile = fileName 
-    logicFile = fileName.replace('n', 'l')
-    solutionFile = fileName.replace('n','s')
+    fileName = input('Input desired number file (ex. f4x4): ')
+    numberFile = fileName + "n.txt"
+    logicFile = numberFile.replace('n', 'l')
+    solutionFile = numberFile.replace('n','s')
 
     nums = []
     log = []
@@ -25,20 +27,25 @@ def ReadIn():
 
     f = open(numberFile, 'r')
     for line in f.readlines():
-        nums.append(line.split(' '))
-
+        x = line
+        x = x.strip()
+        nums.append(x)
+        
     f = open(logicFile, 'r')
     for line in f.readlines():
-        log.append(line.split(' '))
+        x = line
+        x = x.strip()
+        log.append(x)
 
     f = open(solutionFile, 'r')
     for line in f.readlines():
-        sol.append(line.split(' '))
+        x = line
+        x = x.strip()
+        sol.append(x)
 
     return nums, log, sol
 
 
 main()
-=======
 
 
