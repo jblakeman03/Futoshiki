a2ec6aa22716a2f57632190272c03318e392982d
jblakeman03 <jblakeman@murraystate.edu>
2023-09-26 23:26:42 -0500

    Built init Pop method

--- Futoshiki.py
+++ Futoshiki.py
@@ -2,15 +2,15 @@
 ##Futoshiki Solver 
 
 import numpy as np
-import os
+import random
+
 numbers = []
 logic = []
 solution = []
 def main():
+    ##Gets arrays from read in 
     numbers,logic,solution = ReadIn()
-    print(numbers)
-    print(logic)
-    print(solution)
+    ##Create the initial population
     initPop(numbers)
 
 
@@ -18,17 +18,19 @@ def main():
 def ReadIn():
     ##Get the file name for the numbers
     ##Will then switch letters to get desired logic and solution files 
-    fileName = input('Input desired number file (ex. f4x4): ')
-    numberFile = fileName + "n.txt"
+    ##fileName = input('Input desired number file (ex. f4x4): ')
+    numberFile = 'f3x3' + "n.txt"
     logicFile = numberFile.replace('n', 'l')
     solutionFile = numberFile.replace('n','s')
 
+    ##Initialize local temp array variables
     nums = []
     log = []
     sol = []
     rows = 0
     logRows = 0
 
+    ##Opens number files, splits it by line, and adds it to a 1D array and keeps track of row count
     f = open(numberFile, 'r')
     for line in f.readlines():
         x = line
@@ -37,6 +39,7 @@ def ReadIn():
         nums.append(x)
         rows = rows+1
         
+    ##Opens Logic files, splits it by line, and adds it to a 1D array and keeps track of row count
     f = open(logicFile, 'r')
     for line in f.readlines():
         x = line
@@ -44,6 +47,8 @@ def ReadIn():
         x = x.split(' ')
         log.append(x)
         logRows = logRows + 1
+
+    #Opens solution file, splits it by line, and adds it to a 1D array 
     f = open(solutionFile, 'r')
     for line in f.readlines():
         x = line
@@ -51,18 +56,31 @@ def ReadIn():
         x = x.split(' ')
         sol.append(x)
 
+    ##uses row count and turns 1D array into 2D array for all arrays
     nums = np.array(nums).reshape(rows,rows)
     log = np.array(log).reshape(logRows,4)
     sol = np.array(sol).reshape(rows,rows)
 
-
+    ##Returns temp arrays
     return nums, log, sol
 
+##Creates the initial population with random values
 def initPop(nums):
-    nums = np.array(nums)
-    print(np.shape(nums))
-
- 
+    ##Create copy of nums array and casts values as ints
+    nums = np.array(nums, dtype= int)
+    ##Gets number of rows and columns
+    rows, col = np.shape(nums)
+    ##Sets the population size
+    popSize = 10
+    ##Creates 3D array that holds individual elements that represent members of the popultion
+    ##Loops through each 2D matrix and inserts a random value if there isnt a number given
+    pop = np.zeros((popSize, rows, col))
+    for i in range(popSize):
+        for j in range(rows):
+            for k in range(col):
+                if(pop[i][j][k]==0):
+                    pop[i][j][k] = random.randint(1,rows)   
+    print(pop)
 
 main()
 
