3c3d9250b48fb9e4167c04f8651a0411172b7dbf
jblakeman03 <jblakeman@murraystate.edu>
2023-09-26 22:15:44 -0500

    Fixed read in to be numpy compatible

--- Futoshiki.py
+++ Futoshiki.py
@@ -1,16 +1,18 @@
 ##EGR425
 ##Futoshiki Solver 
 
-
+import numpy as np
 import os
 numbers = []
 logic = []
 solution = []
 def main():
-    number,logic,solution = ReadIn()
-    print(number)
+    numbers,logic,solution = ReadIn()
+    print(numbers)
     print(logic)
     print(solution)
+    initPop(numbers)
+
 
 ##Function to read in values and returns them in 2D Arrays  
 def ReadIn():
@@ -24,27 +26,43 @@ def ReadIn():
     nums = []
     log = []
     sol = []
+    rows = 0
+    logRows = 0
 
     f = open(numberFile, 'r')
     for line in f.readlines():
         x = line
         x = x.strip()
+        x = x.split(' ')
         nums.append(x)
+        rows = rows+1
         
     f = open(logicFile, 'r')
     for line in f.readlines():
         x = line
         x = x.strip()
+        x = x.split(' ')
         log.append(x)
-
+        logRows = logRows + 1
     f = open(solutionFile, 'r')
     for line in f.readlines():
         x = line
         x = x.strip()
+        x = x.split(' ')
         sol.append(x)
+
+    nums = np.array(nums).reshape(rows,rows)
+    log = np.array(log).reshape(logRows,4)
+    sol = np.array(sol).reshape(rows,rows)
+
 
     return nums, log, sol
 
+def initPop(nums):
+    nums = np.array(nums)
+    print(np.shape(nums))
+
+ 
 
 main()
 
